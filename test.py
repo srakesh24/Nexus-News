86a40165aae26b72f6b5c20fa3cda7535d9ecb6e
Sampreeth C H <sampreethchsampreethch@gmail.com>
2026-05-13 16:09:02 +0530

    Remove AgentState class and related imports from test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import operator
-from typing import Annotated,List,TypedDict
-from langchain_core.messages import BaseMessage,HumanMessage,SystemMessage
-from langgraph.graph import StateGraph,END
-from langchain_ollama import ChatOllama
-class AgentState(TypedDict):
-    messages: Annotated[List[BaseMessage],operator.add]
-    research_data:List[str]
-    chart_data:List[dict]
