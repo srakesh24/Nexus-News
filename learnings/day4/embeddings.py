2622e3bbddde01b10fd8bdebc302e68cd9eb69c0
Vishnu033 <132803974+Vishnu033@users.noreply.github.com>
2026-05-14 15:46:17 +0530

    Add embeddings implementation using SentenceTransformers

---
+++ learnings/day4/embeddings.py
@@ -0,0 +1,11 @@
+from sentence_transformers import SentenceTransformer 
+import numpy as np
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+sentences = ["Zomato delivers food to your doorstep.", "Python is a programming language.",]
+
+embeddings = model.encode(sentences)
+
+print(f"Shape: {embeddings.shape}")
+
+print(f"First Embedding (First 5 values): {embeddings[0][:5]}...")
